fcc5a14e95a1edbba60ee0c8706860860d138f10
Gabriel Mihai <mihai95gabrieldev@gmail.com>
2022-12-04 14:54:23 +0100

    testing router 2

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Home</h1>
-  <router-link to="/login"></router-link>
+  <router-link to="/login"><btn class="bg-slate-500 ml-10 p-2 rounded-lg">Login</btn></router-link>
 </template>
 
 <script setup></script>
